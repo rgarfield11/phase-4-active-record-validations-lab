f6b60e629c75086bf606b2677bd7a6cd780d7597
Ross <rossgarfield@gmail.com>
2022-06-28 10:42:31 -0500

    commit tree 62d97b21d980dde51993a1270f4ec1984fa54ff0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :title_is_clickbaity
+
+    def title_is_clickbaity
+        if title && title.match?(/Won't Believe/)
+        elsif title && title.match?(/Secret/)
+        elsif title && title.match?(/Top \d/)
+        elsif title && title.match?(/Guess/)
+        else errors.add(:title, "Sorry, this title is not clickbait-y enough.")
+        end
+    end
 end
